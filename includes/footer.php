7305868335e4ffa321157828bd3e03664d06b466
RAVINDRA SINGH <ravindradeeg95@gmail.com>
2024-08-12 18:07:36 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,86 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h3>Neelavath<span>.</span></h3>
+              <p>
+              424,Harmony Complex,ICICI Bank Upstairs,<br>
+              DB Road,Coimbatore, Tamil Nadu 641002<br><br>
+                <strong>Phone:</strong> +91 81486 55011<br>
+                <strong>Email:</strong> mailus@neelavathsoftware.in<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="https://www.youtube.com/channel/UCv_QJEHqoo0d9THqOiNTVaw" class="youtube"><i class="bx bxl-youtube"></i></a>
+                <a href="https://in.linkedin.com/company/neelavath-software-solutions" class="facebook"><i class="bx bxl-facebook"></i></a>
+                
+                <a href="https://in.linkedin.com/company/neelavath-software-solutions" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Blog</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Java Training</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Python Training</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Internship Training</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">AWS Training</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Final Year Project</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Our Newsletter</h4>
+            <p>Subcribe to get our regular offers on your mailbox..</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Neelavath</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">Designed by <a href="http://www.neelavathsoftware.in/">Neelavath Software</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,119 @@
+<!-- Favicons -->
+<link href="assets/img/favicon.ico" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center justify-content-lg-between">
+
+      <h1 class="logo me-auto me-lg-0"><a href="index.html">Neelavath<span>.</span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="index.php">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li class="dropdown"><a href="#"><span>Services</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li class="dropdown"><a href="#"><span>Development</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="Website-development-in-coimbatore.php">Creative Web Development</a></li>
+                  <li><a href="software-development-in-coimbatore.php">Mobile Application Development</a></li>
+                  <li><a href="software-development-in-coimbatore.php">Software Development</a></li>
+                  <li><a href="software-development-in-coimbatore.php">Digital Marketing</a></li>
+                  <li><a href="software-development-in-coimbatore.php">Graphic Designs</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>Internship</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="web-development-internship.php">Web Design Internship</a></li>
+                  <li><a href="Software-internship-in-coimbatore.php">Software Internship</a></li>
+                  <li><a href="#services">Final Year Internship</a></li>
+                  <li><a href="#services">In Plant Training</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>Software Training & Certifications</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#services">Java Training</a></li>
+                  <li><a href="#services">Dotnet Training</a></li>
+                  <li><a href="#services">Python Training</a></li>
+                  <li><a href="#services">Android Training</a></li>
+                  <li><a href="#services">PHP Training</a></li>
+                  <li><a href="#services">AWS Training</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Final Year Projects</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li class="dropdown"><a href="#"><span>IEEE Projects</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#services">Cloud Computing Projects</a></li>
+                  <li><a href="#services">Networking Projects</a></li>
+                  <li><a href="#services">Mobile Computing Projects</a></li>
+                  <li><a href="#services">Machine Learning Projects</a></li>
+                  <li><a href="#services">Artificial Intelligence Projects</a></li>                  
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>Application Projects</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#services">Java Projects</a></li>
+                  <li><a href="#services">Dotnet Projects</a></li>
+                  <li><a href="#services">Python Projects</a></li>
+                  <li><a href="#services">Android Projects</a></li>
+                  <li><a href="#services">PHP Projects</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>Non-It Projects</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#services">Matlab Projects</a></li>
+                  <li><a href="#services">Embedded Projects</a></li>
+                  <li><a href="#services">IOT Projects</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>Application Projects</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#services">Web Application Projects</a></li>
+                  <li><a href="#services">Mobile Application Projects</a></li>
+                  <li><a href="#services">Desktop Application Projects</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>MBA Projects</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#services">Marketing Projects</a></li>
+                  <li><a href="#services">HR Projects</a></li>
+                  <li><a href="#services">Finance Projects</a></li>
+                  <li><a href="#services">Operations Projects</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#team">Blog</a></li>          
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="#about" class="get-started-btn scrollto">Get Started</a>
+
+    </div>
+  </header><!-- End Header -->
